4a065a1de364b19077fe3c38eaeeecac88b0e465
agustinbarros13 <agustin-b_@outlook.es>
2024-07-03 13:16:53 +0200

    modificación final

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -21,9 +21,20 @@ export function renderSearchForm() {
 
 async function fetchImages(query) {
   const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY
-  const response = await fetch(
+  let response = await fetch(
     `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`
   )
-  const data = await response.json()
+  let data = await response.json()
+
+  if (data.results.length === 0 && query !== 'gatos') {
+    response = await fetch(
+      `https://api.unsplash.com/search/photos?query=gatos&client_id=${accessKey}`
+    )
+    data = await response.json()
+    alert(
+      'No se encontraron imágenes con la palabra introducida. Se muestran imágenes de gatos como alternativa.'
+    )
+  }
+
   return data.results
 }
